ed60238ca5f640cb961af7314206933b535ba4f5
pongpanott <fhyptr@gmail.com>
2023-02-20 15:53:39 +0700

    chore: config repo for development

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} \*/
+
+module.exports = {
+	content: ["./src/**/*.{ts,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["var(--font-apercu)", ...fontFamily.sans],
+			},
+		},
+	},
+	plugins: [],
+};
